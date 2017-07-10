473eccd24510b7354a1cab3d1d08cfaf28bdf0d8
saurabhverma0609 <saurabh-verma@outlook.com>
2017-07-10 13:04:51 +0530

    Session 1 assignment

---
+++ src/corejava/introToJava/Assingments/Assignment1.java
@@ -0,0 +1,19 @@
+/*
+ * Problem Statement :Write a program to print Hello World.
+ */
+
+package corejava.introToJava.Assingments;
+
+//Class initialization
+
+public class Assignment1 {
+
+	public static void main(String[] args) {
+
+// System.out.println is used to display output on console.
+		
+		System.out.println("Hello World.");
+
+	}
+
+}
